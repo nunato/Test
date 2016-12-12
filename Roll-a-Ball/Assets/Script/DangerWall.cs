919b4d428d38a83c7bae0fc01559f3dd76e5d7a6
nunato <kyrie601@yahoo.co.jp>
2016-12-13 00:42:35 +0900

    text

---
+++ Roll-a-Ball/Assets/Script/DangerWall.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DangerWall : MonoBehaviour
+{
+	//オブジェクトと接触したときに呼ばれるコールバック
+	void OnCollisionEnter( Collision hit )
+	{
+		//接触したオブジェクトのタグがPlayerの場合
+		if( hit.gameObject.CompareTag( "Player" ) ){
+			//現在のシーン番号を取得
+			int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+			
+			//現在のシーンを再読み込みする
+			SceneManager.LoadScene( sceneIndex );
+		}
+	}
+}
---
+++ Roll-a-Ball/Assets/Script/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour
+{
+	//ターゲットへの参照
+	public Transform target;
+	//相対座標
+	private Vector3 offset;
+	
+	void Start()
+	{
+		//自分自身とtargetとの相対座標を求める
+		offset = GetComponent<Transform>().position - target.position;
+	}
+	
+	void Update()
+	{
+		//自分の座標にtargetの座標を代入する
+		GetComponent<Transform>().position = target.position + offset;
+	}
+}
